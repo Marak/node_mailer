25d2e0c26ff0033747674ac0535789656672cefe
Tim-Smart <tim@fostle.com>
2010-06-20 11:56:08 +0800

    A few whitespace changes

--- node_mailer.js
+++ node_mailer.js
@@ -25,82 +25,80 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 /********* USAGE ***********
 
-	var email = require("./node_mailer");
-	email.send({
-	  to : "marak.squires@gmail.com",
-	  from : "obama@whitehouse.gov",
-	  subject : "node_mailer test email",
-	  body : "hello this is a test email from the node_mailer"
-	});
-	
+  var email = require("./node_mailer");
+  email.send({
+    to : "marak.squires@gmail.com",
+    from : "obama@whitehouse.gov",
+    subject : "node_mailer test email",
+    body : "hello this is a test email from the node_mailer"
+  });
+
 ****************************/
 
-var tcp = require('tcp');
+var tcp = require('net');
 var sys = require('sys');
 
-
 var email = {
-  send:function (options){
+  send: function (options) {
     var options = typeof(options) == "undefined" ? {} : options;
-	options.to = typeof(options.to) == "undefined" ? "marak.squires@gmail.com" : options.to;
-	options.from = typeof(options.from) == "undefined" ? "obama@whitehouse.gov" : options.from;
-	options.subject = typeof(options.subject) == "undefined" ? "node_mailer test email" : options.subject;
-	options.body = typeof(options.body) == "undefined" ? "hello this is a test email from the node_mailer" : options.body;	
-		
-	var self = this;
+    options.to = typeof(options.to) == "undefined" ? "marak.squires@gmail.com" : options.to;
+    options.from = typeof(options.from) == "undefined" ? "obama@whitehouse.gov" : options.from;
+    options.subject = typeof(options.subject) == "undefined" ? "node_mailer test email" : options.subject;
+    options.body = typeof(options.body) == "undefined" ? "hello this is a test email from the node_mailer" : options.body;  
+        
+    var self = this;
 
-	this.connection = tcp.createConnection(25);
-	this.connection.addListener("connect", function (socket) {
-	  self.connection.write("helo localhost\r\n");
-	  self.connection.write("mail from: " + options.from + "\r\n");
-	  self.connection.write("rcpt to: " + options.to + "\r\n");
-	  self.connection.write("data\r\n");
-	  self.connection.write("From: " + options.from + "\r\n");
-	  self.connection.write("To: " + options.to + "\r\n");
-	  self.connection.write("Subject: " + options.subject + "\r\n");
-	  self.connection.write("Content-Type: text/html\r\n");
-	  self.connection.write(email.wordwrap(options.body) + "\r\n");
-	  self.connection.write(".\r\n");
-	  self.connection.write("quit\r\n");
-	  self.connection.end();
-	});
+    this.connection = tcp.createConnection(25);
+    this.connection.setEncoding('utf8');
+    this.connection.addListener("connect", function () {
+      self.connection.write("helo localhost\r\n");
+      self.connection.write("mail from: " + options.from + "\r\n");
+      self.connection.write("rcpt to: " + options.to + "\r\n");
+      self.connection.write("data\r\n");
+      self.connection.write("From: " + options.from + "\r\n");
+      self.connection.write("To: " + options.to + "\r\n");
+      self.connection.write("Subject: " + options.subject + "\r\n");
+      self.connection.write("Content-Type: text/html\r\n");
+      self.connection.write(email.wordwrap(options.body) + "\r\n");
+      self.connection.write(".\r\n");
+      self.connection.write("quit\r\n");
+      self.connection.end();
+    });
 
-	this.connection.addListener("data", function (data) {
-		if(email.parseResponse(data)){
-
-			sys.puts("SUCC");
-		}else{
-
-			sys.puts("ERR");
-		}
-		sys.puts(data);
-	});
+    this.connection.addListener("data", function (data) {
+        if(email.parseResponse(data)){
+          sys.puts("SUCC");
+        } else{
+          sys.puts("ERR");
+        }
+        sys.puts(data);
+    });
   },
 
   parseResponse:function(data){
     var d = data.split("\r\n");
-	d.forEach(function(itm){
-  	  if(itm.indexOf("250 OK id=") != -1){
-	    return true;
-	  }
-	});
+    d.forEach(function(itm){
+      if(itm.indexOf("250 OK id=") != -1){
+        return true;
+      }
+    });
     return false;
   },
   
   wordwrap:function(str){
     var m = 80;
-	var b = "\r\n";
-	var c = false;
-	var i, j, l, s, r;
-	str += '';
-	if (m < 1) {
- 	  return str;
-	}
-	for (i = -1, l = (r = str.split(/\r\n|\n|\r/)).length; ++i < l; r[i] += s) {
-	  for(s = r[i], r[i] = ""; s.length > m; r[i] += s.slice(0, j) + ((s = s.slice(j)).length ? b : "")){
-	    j = c == 2 || (j = s.slice(0, m + 1).match(/\S*(\s)?$/))[1] ? m : j.input.length - j[0].length || c == 1 && m || j.input.length + (j = s.slice(m).match(/^\S*/)).input.length;
-	  }
-	}
+    var b = "\r\n";
+    var c = false;
+    var i, j, l, s, r;
+    str += '';
+    if (m < 1) {
+      return str;
+    }
+    for (i = -1, l = (r = str.split(/\r\n|\n|\r/)).length; ++i < l; r[i] += s) {
+      for(s = r[i], r[i] = ""; s.length > m; r[i] += s.slice(0, j) + ((s = s.slice(j)).length ? b : "")){
+        j = c == 2 || (j = s.slice(0, m + 1).match(/\S*(\s)?$/))[1] ? m : j.input.length - j[0].length || c == 1 && m || j.input.length + (j = s.slice(m).match(/^\S*/)).input.length;
+      }
+    }
     return r.join("\n");
   }
 }

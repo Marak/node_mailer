67c2ee8bd35768e75ca51c9d14e83cfab2d246d8
Marak Squires <marak.squires@gmail.com>
2010-09-14 03:08:49 -0400

    [api] connection pool is closer to working. new connections are now created for every message

--- demo.js
+++ demo.js
@@ -2,21 +2,21 @@ var email = require("./lib/node_mailer");
 
 for(var i = 0; i < 5; i++){
 
+  email.send({
+    host : "localhost",              // smtp server hostname
+    port : "1025",                     // smtp server port
+    domain : "localhost",            // domain used by client to identify itself to server
+    to : "marak.squires@gmail.com",
+    from : "obama@whitehouse.gov",
+    subject : "node_mailer test email",
+    body : "hello this is a test email from the node_mailer",
+
+    authentication : "login",        // auth login is supported; anything else is no auth
+    username : "dXNlcm5hbWU=",       // Base64 encoded username
+    password : "cGFzc3dvcmQ=",       // Base64 encoded password
+
+  });
 
 
 }
 
-email.send({
-  host : "localhost",              // smtp server hostname
-  port : "1025",                     // smtp server port
-  domain : "localhost",            // domain used by client to identify itself to server
-  to : "marak.squires@gmail.com",
-  from : "obama@whitehouse.gov",
-  subject : "node_mailer test email",
-  body : "hello this is a test email from the node_mailer",
-  
-  authentication : "login",        // auth login is supported; anything else is no auth
-  username : "dXNlcm5hbWU=",       // Base64 encoded username
-  password : "cGFzc3dvcmQ=",       // Base64 encoded password
-  
-});

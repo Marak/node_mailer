9309ac34932a9a157c11d8a7ab03f27f6226a499
Marak Squires <marak.squires@gmail.com>
2010-09-14 03:57:48 -0400

    [api] connection pooling per server and port seems to be working now.

--- demo.js
+++ demo.js
@@ -18,5 +18,21 @@ for(var i = 0; i < 5; i++){
   });
 
 
+  email.send({
+    host : "localhost",              // smtp server hostname
+    port : "1026",                     // smtp server port
+    domain : "localhost",            // domain used by client to identify itself to server
+    to : "marak.squires@gmail.com",
+    from : "obama@whitehouse.gov",
+    subject : "node_mailer test email 2",
+    body : "hello this is a test email from the node_mailer",
+
+    authentication : "login",        // auth login is supported; anything else is no auth
+    username : "dXNlcm5hbWU=",       // Base64 encoded username
+    password : "cGFzc3dvcmQ=",       // Base64 encoded password
+
+  });
+
+
 }
 

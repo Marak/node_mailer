ca611674cf86d7c795d97d135716065556210b90
Marak Squires <marak.squires@gmail.com>
2010-11-01 16:23:08 -0400

    updated demo and docs, bumped to version 0.3.0

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 var email = require("./lib/node_mailer");
 
-for(var i = 0; i < 5; i++){
+for(var i = 0; i < 10; i++){
 
   email.send({
     host : "localhost",              // smtp server hostname
@@ -9,23 +9,7 @@ for(var i = 0; i < 5; i++){
     to : "marak.squires@gmail.com",
     from : "obama@whitehouse.gov",
     subject : "node_mailer test email",
-    body : "hello this is a test email from the node_mailer",
-
-    authentication : "login",        // auth login is supported; anything else is no auth
-    username : "dXNlcm5hbWU=",       // Base64 encoded username
-    password : "cGFzc3dvcmQ=",       // Base64 encoded password
-
-  });
-
-
-  email.send({
-    host : "localhost",              // smtp server hostname
-    port : "1026",                     // smtp server port
-    domain : "localhost",            // domain used by client to identify itself to server
-    to : "marak.squires@gmail.com",
-    from : "obama@whitehouse.gov",
-    subject : "node_mailer test email 2",
-    body : "hello this is a test email from the node_mailer",
+    body : "hello this the " + i + " a test email from the node_mailer",
 
     authentication : "login",        // auth login is supported; anything else is no auth
     username : "dXNlcm5hbWU=",       // Base64 encoded username

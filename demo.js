c3d74c083d991c1c0a1af7c132b099179fb153ef
Bradley Meck <bradley.meck@gmail.com>
2011-04-29 01:17:27 -0500

    removed template logic, using Nodemailer as backing library
    Edited demo.js via GitHub

--- demo.js
+++ demo.js
@@ -1,18 +1,18 @@
 var email = require("./lib/node_mailer");
 
 for(var i = 0; i < 10; i++){
-
   email.send({
-    host : "localhost",              // smtp server hostname
-    port : "25",                     // smtp server port
-    domain : "localhost",            // domain used by client to identify itself to server
-    to : "marak.squires@gmail.com",
-    from : "obama@whitehouse.gov",
+    ssl: true,
+    host : "smtp.gmail.com",              // smtp server hostname
+    port : 465,                     // smtp server port
+    domain : "[127.0.0.1]",            // domain used by client to identify itself to server
+    to : "bradley.meck@gmail.com",
+    from : "bradley.meck@gmail.com",
     subject : "node_mailer test email",
     body: "Hello! This is a test of the node_mailer.",
     authentication : "login",        // auth login is supported; anything else is no auth
-    username : "dXNlcm5hbWU=",       // Base64 encoded username
-    password : "cGFzc3dvcmQ="       // Base64 encoded password
+    username : undefined,       // Base64 encoded username
+    password : undefined      // Base64 encoded password
   },
   function(err, result){
     if(err){ console.log(err); }
